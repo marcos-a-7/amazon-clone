a11ad41767b317c774f1c9fad540d5acacb5ee91
Marcos <marcos.aguilera.7.13@gmail.com>
2024-01-13 20:18:23 -0300

    new products hardcoded

--- src/Home.js
+++ src/Home.js
@@ -13,29 +13,52 @@ function Home() {
         />
         <div className="home__row">
           <Product
-            id={123456}
-            title="The lean startup"
-            price="19.99"
+            id={1}
+            title="SAMSUNG Odyssey G70B Series 32-Inch 4K UHD Gaming Monitor, IPS Panel, 144Hz"
+            price="649.99"
             rating={5}
-            image="https://images-na.ssl-images-amazon.com/images/G/01/AmazonExports/Events/2023/EBF23/Fuji_Desktop_Single_image_EBF_1x_v1._SY304_CB573698005_.jpg"
+            image="https://m.media-amazon.com/images/I/41etBiSd9jS._AC_UF452,452_FMjpg_.jpg"
           />
           <Product
-            id={123456}
-            title="The lean startup"
-            price="19.99"
-            rating={5}
-            image="https://images-na.ssl-images-amazon.com/images/G/01/AmazonExports/Events/2023/EBF23/Fuji_Desktop_Single_image_EBF_1x_v1._SY304_CB573698005_.jpg"
+            id={2}
+            title="TP-Link Deco BE33000 Quad-Band WiFi 7 Mesh System (Deco BE95) for Whole Home Coverage up to 7800 Sq.Ft with AI-Driven Smart Antennas"
+            price="999.99"
+            rating={4}
+            image="https://m.media-amazon.com/images/I/313Uy44WCzL._AC_UF452,452_FMjpg_.jpg"
           />
         </div>
 
         <div className="home__row">
-          <Product />
-          <Product />
-          <Product />
+          <Product
+            id={3}
+            title="AMD Ryzen 9 5950X 16-core, 32-thread unlocked desktop processor"
+            price="399.00"
+            rating={4}
+            image="https://m.media-amazon.com/images/I/21bcVc77fEL._AC_UF226,226_FMjpg_.jpg"
+          />
+          <Product
+            id={4}
+            title="ZOTAC Gaming GeForce RTX™ 3060 Ti Twin Edge OC LHR 8GB GDDR6 256-bit 14 Gbps PCIE 4.0 Graphics Card"
+            price="349.99"
+            rating={5}
+            image="https://m.media-amazon.com/images/I/41QcAwEXIgL._AC_UF226,226_FMjpg_.jpg"
+          />
+          <Product
+            id={6}
+            title="Realspace® Fennington Bonded Leather High-Back Chair, Brown/Black"
+            price="169.99"
+            rating={3}
+            image="https://m.media-amazon.com/images/I/71c8TWr8EYL._AC_UL320_.jpg"
+          />
         </div>
-
         <div className="home__row">
-          <Product />
+          <Product
+            id={5}
+            title="Corsair Dark Core RGB Pro SE, FPS/MOBA Gaming Mouse with SLIPSTREAM Technology, Black, Backlit RGB LED"
+            price="79.99"
+            rating={5}
+            image="https://m.media-amazon.com/images/I/41S0Q1AjqWL._AC_UF226,226_FMjpg_.jpg"
+          />
         </div>
       </div>
     </div>
